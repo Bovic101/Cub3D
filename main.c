d1b43748c6ee6d3c1c91cd05056671462c0ecdc8
tkirmizi <t.kirmizioglu@outlook.com>
2024-12-16 12:33:50 +0100

    make started and something to understand 42 mlx

--- main.c
+++ main.c
@@ -6,11 +6,54 @@
 /*   By: tkirmizi <tkirmizi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 19:35:05 by taha              #+#    #+#             */
-/*   Updated: 2024/12/16 11:10:36 by tkirmizi         ###   ########.fr       */
+/*   Updated: 2024/12/16 12:16:51 by tkirmizi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char **av)
-{
-    printf("hello world");
+
+#include "MLX42/MLX42.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    // Pencereyi başlat
+    mlx_t *mlx = mlx_init(800, 600, "so_long", true);
+    if (!mlx)
+        return (fprintf(stderr, "MLX başlatılamadı!\n"), 1);
+
+    // PNG görselini yükle
+    mlx_texture_t *texture = mlx_load_png("player.png");
+    if (!texture) {
+        fprintf(stderr, "Görsel yüklenemedi!\n");
+        mlx_terminate(mlx);
+        return 1;
+    }
+
+    // Texture'ı pencereye doğrudan yerleştirmek için image'e dönüştür
+    mlx_image_t *img = mlx_new_image(mlx, texture->width, texture->height);
+    if (!img) {
+        fprintf(stderr, "Image oluşturulamadı!\n");
+        mlx_delete_texture(texture);
+        mlx_terminate(mlx);
+        return 1;
+    }
+
+    // Texture'ı image'e kopyala
+    mlx_texture_to_image(mlx, texture);
+
+    // Görseli pencereye yerleştir
+    mlx_image_to_window(mlx, img, 200, 200);
+
+    // Texture'ı serbest bırak
+    mlx_delete_texture(texture);
+
+    // Pencereyi başlat
+    mlx_loop(mlx);
+
+    // Kaynakları temizle
+    mlx_terminate(mlx);
+    return 0;
 }
+
+
+
---
+++ makefile
@@ -0,0 +1,40 @@
+NAME = cup3d
+
+LIBMLX = ./MLX42
+
+HEADERS = -I ./mlx/include -I $(LIBMLX)/include
+SRCS = main.c
+LIBS = $(LIBMLX)/build/libmlx42.a -ldl -lglfw -pthread -lm
+OBJS = $(SRCS:.c=.o)
+CC = gcc
+CFLAGS = -Wunreachable-code -Ofast -g
+
+all: libmlx $(NAME)
+
+libmlx:
+	@if [ ! -d "MLX42" ]; then \
+		git clone https://github.com/codam-coding-college/MLX42.git MLX42; \
+	else \
+		echo "Already have"; \
+	fi
+		@cd MLX42 && cmake -B build && cmake --build build -j4 \
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) $(LIBS) $(HEADER) -o $(NAME)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@ $(HEADERS)
+
+clean:
+	rm -f $(OBJS)
+	rm -rf $(LIBMLX)/build
+	make -C lib clean
+
+fclean: clean
+	rm -f $(NAME)
+	make -C lib fclean
+	rm -rf MLX42
+
+re: fclean all
+
+.PHONY: all clean fclean re libmlx
