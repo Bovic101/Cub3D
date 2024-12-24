329f3f4f09d7b0cf0de074a1591ed9ebde7860e6
vodebunm <vodebunm@student.42heilbronn.de>
2024-12-24 10:44:21 +0100

    Header implementation

--- cup3d.h
+++ cup3d.h
@@ -3,10 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   cup3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkirmizi <tkirmizi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 12:07:34 by tkirmizi          #+#    #+#             */
-/*   Updated: 2024/12/16 12:07:43 by tkirmizi         ###   ########.fr       */
+/*   Updated: 2024/12/24 10:41:36 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "MLX42/MLX42.h"
+#include <math.h>
+
+#define DISPLAY_HEIGHT 900
+#define DISPLAY_WIDTH	1600
+#define BLOCK_SIZE 40
+#define VIEW_ANGLE 45
+#define PLAYER_MOVE_SPEED 3 //Player unit- movement per frame 
+#define TURNING_SPEED 0.1//turning speed can be change
+
+//Structure for player data and map
+typedef struct s_map_data
+{
+	int		player_block_x;
+	int		player_block_y;
+	int		map_width;
+	int		map_height;
+	char	**map_layout;//pointer of 2D array
+}	t_map_data;
+
+//Structure of player data
+typedef struct s_player
+{
+	int		x_cordinate; //Player horizontal coordinate in pixel
+	int		y_cordinate; //Player horizontal coordinate in pixel
+	int		player_rotate; //Player roration flag
+	double	direction; //Player's facing direction
+	float	field_view;//Player viewpoint at any time(radian)
+	int		move_updown;
+	int		move_leftright;
+}	t_player_data;
+
+typedef struct s_mlx
+{
+	mlx_image_t		*game_image; //Image to render the game
+	mlx_instance_t	*instances; //Instance to manage window
+	t_player_data	*player; //player data
+	t_map_data		*s_map_data; //map containing map and player info.
+}	t_mlx_render;
