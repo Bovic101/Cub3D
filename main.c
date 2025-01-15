9ca1da7ff69ba86fc9f9e56d1f540d0ac6246319
Bovic101 <Victorbolanle72@gmail.com>
2025-01-15 14:51:06 +0100

    Fixing of segmentation fault that occur in the ft_render_wall and ft_ray_cast + inclusion of more config file for map

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taha <taha@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: victor-linux <victor-linux@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 19:35:05 by taha              #+#    #+#             */
-/*   Updated: 2025/01/15 12:43:59 by taha             ###   ########.fr       */
+/*   Updated: 2025/01/15 14:20:05 by victor-linu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_game game;
+	t_game	game;
 
 	if (argc != 2)
 	{
@@ -45,7 +45,7 @@ int main(int argc, char **argv)
 	return (0);
 }
 
-void init_window(t_game *game)
+void	init_window(t_game *game)
 {
 	game->mlx = mlx_init(DISPLAY_WIDTH, DISPLAY_HEIGHT, "Cub3D", true);
 	if (!game->mlx)
@@ -102,7 +102,7 @@ void	rotate_player(t_game *game)
 	}
 }
 
-int init_game(t_game *game, const char *map_file)
+int	init_game(t_game *game, const char *map_file)
 {
 	memset(game, 0, sizeof(t_game));
 	game->p = malloc(sizeof(t_player_data));
@@ -172,9 +172,9 @@ void	cleanup_game(t_game *game)
 		mlx_terminate(game->mlx);
 }
 
-void key_handler(mlx_key_data_t keydata, void *param)
+void	key_handler(mlx_key_data_t keydata, void *param)
 {
-	t_game *game;
+	t_game	*game;
 
 	game = (t_game *)param;
 	if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
@@ -198,7 +198,7 @@ void key_handler(mlx_key_data_t keydata, void *param)
 		mlx_close_window(game->mlx);
 }
 
-void key_handler_sec(mlx_key_data_t keydata, t_game *game)
+void	key_handler_sec(mlx_key_data_t keydata, t_game *game)
 {
 	if (keydata.key == MLX_KEY_W)
 		game->p->m_up = 0;
@@ -212,10 +212,10 @@ void key_handler_sec(mlx_key_data_t keydata, t_game *game)
 		game->p->p_rot = 0;
 }
 
-void move_player_forward_back(t_game *game, double move_speed)
+void	move_player_forward_back(t_game *game, double move_speed)
 {
-	double new_x;
-	double new_y;
+	double	new_x;
+	double	new_y;
 
 	if (game->p->m_up)
 	{
@@ -266,26 +266,26 @@ void	move_player_left_right(t_game *game, double move_speed)
 	}
 }
 
-int is_valid_position(t_game *game, double x, double y)
+int	is_valid_position(t_game *game, double x, double y)
 {
-	int map_x;
-	int map_y;
+	int	map_x;
+	int	map_y;
 
 	map_x = (int)(x / BLOCK_SIZE);
 	map_y = (int)(y / BLOCK_SIZE);
-	if (map_x < 0 || map_y < 0 || 
-		map_y >= game->mapdata->map_height || 
+	if (map_x < 0 || map_y < 0 ||
+		map_y >= game->mapdata->map_height ||
 		map_x >= (int)strlen(game->mapdata->map_layout[map_y]))
-			return (0);
+		return (0);
 	if (game->mapdata->map_layout[map_y][map_x] == '1')
 		return (0);
 	return (1);
 }
 
-void move_player(t_game *game)
+void	move_player(t_game *game)
 {
-	static int debug_counter;
-	double move_speed;
+	static int	debug_counter;
+	double		move_speed;
 
 	debug_counter = 0;
 	move_speed = PLAYER_MOVE_SPEED;
@@ -306,26 +306,28 @@ void	ft_draw_ceiling_floor(t_game *game)
 		while (++x < DISPLAY_WIDTH)
 		{
 			if (y < DISPLAY_HEIGHT / 2)
-				((uint32_t *)game->screen->pixels)[y * DISPLAY_WIDTH + x] = game->mlx_r->ceiling_c;
+				((uint32_t *)game->screen->pixels)[y * DISPLAY_WIDTH
+					+ x] = game->mlx_r->ceiling_c;
 			else
-				((uint32_t *)game->screen->pixels)[y * DISPLAY_WIDTH + x] = game->mlx_r->floor_c;
+				((uint32_t *)game->screen->pixels)[y * DISPLAY_WIDTH
+					+ x] = game->mlx_r->floor_c;
 		}
 	}
 }
 
-void game_loop(void *param)
+void	game_loop(void *param)
 {
-	t_game *game;
+	t_game	*game;
+	int		x;
+	int		y;
+
 	game = (t_game *)param;
-	int	x;
-	int	y;
-	
 	y = -1;
 	x = -1;
 	if (!game || !game->screen || !game->screen->pixels)
 	{
 		printf("Invalid game state in game loop\n");
-		return;
+		return ;
 	}
 	move_player(game);
 	ft_draw_ceiling_floor(game);
@@ -336,94 +338,116 @@ void game_loop(void *param)
 
 void	ft_cast_rays(t_game *game, int x, int y)
 {
+	if (!game || !game->rc || !game->p)
+	{
+		write(2, "Error: Invalid game structure\n", 30);
+		return ;
+	}
+	x = -1;
 	while (++x < DISPLAY_WIDTH)
 	{
 		game->rc->camera_x = 2 * x / (double)DISPLAY_WIDTH - 1;
-		game->rc->ray_dir_x = game->p->dir_x + game->p->plane_x * game->rc->camera_x;
-		game->rc->ray_dir_y = game->p->dir_y + game->p->plane_y * game->rc->camera_x;
+		game->rc->ray_dir_x = game->p->dir_x + game->p->plane_x
+			* game->rc->camera_x;
+		game->rc->ray_dir_y = game->p->dir_y + game->p->plane_y
+			* game->rc->camera_x;
 		game->rc->map_x = (int)(game->p->pos_x / BLOCK_SIZE);
 		game->rc->map_y = (int)(game->p->pos_y / BLOCK_SIZE);
-	if (game->rc->ray_dir_x == 0)
-		game->rc->delta_dist_x = 1e30;
-	else
-		game->rc->delta_dist_x = fabs(1 / game->rc->ray_dir_x);
-	if (game->rc->ray_dir_y == 0)
-		game->rc->delta_dist_y = 1e30;
-	else
-		game->rc->delta_dist_y = fabs(1 / game->rc->ray_dir_y);
+		game->rc->delta_dist_x = (game->rc->ray_dir_x == 0) ? 1e30 : fabs(1
+				/ game->rc->ray_dir_x);
+		game->rc->delta_dist_y = (game->rc->ray_dir_y == 0) ? 1e30 : fabs(1
+				/ game->rc->ray_dir_y);
 		if (game->rc->ray_dir_x < 0)
 		{
 			game->rc->step_x = -1;
-			game->rc->side_dist_x = (game->p->pos_x / BLOCK_SIZE - game->rc->map_x) * game->rc->delta_dist_x;
+			game->rc->side_dist_x = (game->p->pos_x / BLOCK_SIZE
+					- game->rc->map_x) * game->rc->delta_dist_x;
 		}
 		else
 		{
 			game->rc->step_x = 1;
-			game->rc->side_dist_x = (game->rc->map_x + 1.0 - game->p->pos_x / BLOCK_SIZE) * game->rc->delta_dist_x;
+			game->rc->side_dist_x = (game->rc->map_x + 1.0 - game->p->pos_x
+					/ BLOCK_SIZE) * game->rc->delta_dist_x;
 		}
 		if (game->rc->ray_dir_y < 0)
 		{
 			game->rc->step_y = -1;
-			game->rc->side_dist_y = (game->p->pos_y / BLOCK_SIZE - game->rc->map_y) * game->rc->delta_dist_y;
+			game->rc->side_dist_y = (game->p->pos_y / BLOCK_SIZE
+					- game->rc->map_y) * game->rc->delta_dist_y;
 		}
 		else
 		{
 			game->rc->step_y = 1;
-			game->rc->side_dist_y = (game->rc->map_y + 1.0 - game->p->pos_y / BLOCK_SIZE) * game->rc->delta_dist_y;
+			game->rc->side_dist_y = (game->rc->map_y + 1.0 - game->p->pos_y
+					/ BLOCK_SIZE) * game->rc->delta_dist_y;
 		}
 		ft_perform_dda(game);
 		ft_render_wall(game, x, y);
 	}
 }
 
-
 void	ft_perform_dda(t_game *game)
 {
-		game->rc->hit = 0;
-		while (game->rc->hit == 0)
+	game->rc->hit = 0;
+	while (game->rc->hit == 0)
+	{
+		if (game->rc->side_dist_x < game->rc->side_dist_y)
 		{
-			if (game->rc->side_dist_x < game->rc->side_dist_y)
-			{
-				game->rc->side_dist_x += game->rc->delta_dist_x;
-				game->rc->map_x += game->rc->step_x;
-				game->rc->side = 0;
-			}
-			else
-			{
-				game->rc->side_dist_y += game->rc->delta_dist_y;
-				game->rc->map_y += game->rc->step_y;
-				game->rc->side = 1;
-			}
-			if (game->rc->map_x >= 0 && game->rc->map_y >= 0 && game->rc->map_y < game->mapdata->map_height && 
-				game->rc->map_x < (int)strlen(game->mapdata->map_layout[game->rc->map_y]) && 
-				game->mapdata->map_layout[game->rc->map_y][game->rc->map_x] == '1')
-				game->rc->hit = 1;
+			game->rc->side_dist_x += game->rc->delta_dist_x;
+			game->rc->map_x += game->rc->step_x;
+			game->rc->side = 0;
 		}
+		else
+		{
+			game->rc->side_dist_y += game->rc->delta_dist_y;
+			game->rc->map_y += game->rc->step_y;
+			game->rc->side = 1;
+		}
+		if (game->rc->map_x >= 0 && game->rc->map_y >= 0
+			&& game->rc->map_y < game->mapdata->map_height &&
+			game->rc->map_x < (int)strlen(game->mapdata->map_layout[game->rc->map_y])
+				&&
+			game->mapdata->map_layout[game->rc->map_y][game->rc->map_x] == '1')
+			game->rc->hit = 1;
+	}
 }
 
 void	ft_render_wall(t_game *game, int x, int y)
 {
-			if (game->rc->side == 0)
-			game->rc->perp_wall_dist = (game->rc->side_dist_x - game->rc->delta_dist_x);
-		else
-			game->rc->perp_wall_dist = (game->rc->side_dist_y - game->rc->delta_dist_y);
-		game->rc->line_height = (int)(DISPLAY_HEIGHT /game->rc-> perp_wall_dist);
-		game->rc->draw_start = - game->rc->line_height / 2 + DISPLAY_HEIGHT / 2;
-		if (game->rc->draw_start < 0)
-			game->rc->draw_start = 0;
-		game->rc->draw_end = game->rc->line_height / 2 + DISPLAY_HEIGHT / 2;
-		if (game->rc->draw_end >= DISPLAY_HEIGHT)
-			game->rc->draw_end = DISPLAY_HEIGHT - 1;
-		y = game->rc->draw_start;
-				while (y++ <= game->rc->draw_end)
+	if (!game || !game->screen || !game->screen->pixels)
+	{
+		print_error("Error: Invalid game or screen buffer\n");
+		return ;
+	}
+	if (game->rc->side == 0)
+		game->rc->perp_wall_dist = (game->rc->side_dist_x
+				- game->rc->delta_dist_x);
+	else
+		game->rc->perp_wall_dist = (game->rc->side_dist_y
+				- game->rc->delta_dist_y);
+	game->rc->line_height = (int)(DISPLAY_HEIGHT / game->rc->perp_wall_dist);
+	game->rc->draw_start = -game->rc->line_height / 2 + DISPLAY_HEIGHT / 2;
+	if (game->rc->draw_start < 0)
+		game->rc->draw_start = 0;
+	game->rc->draw_end = game->rc->line_height / 2 + DISPLAY_HEIGHT / 2;
+	if (game->rc->draw_end >= DISPLAY_HEIGHT)
+		game->rc->draw_end = DISPLAY_HEIGHT - 1;
+	y = game->rc->draw_start;
+	while (y <= game->rc->draw_end)
+	{
+		if (y < 0 || y >= DISPLAY_HEIGHT)
 		{
-			game->rc->color = 0xFF0000FF; // gonna change when textures add but now red walls.
-			if (game->rc->side == 1) game->rc->color = 0x800000FF; // y side of walls
-			((uint32_t *)game->screen->pixels)[y * DISPLAY_WIDTH + x] = game->rc->color;
+			print_error("Error: y out of bounds\n");
+			break ;
 		}
+		game->rc->color = 0xFF0000FF;
+		if (game->rc->side == 1)
+			game->rc->color = 0x800000FF;
+		((uint32_t *)game->screen->pixels)[y * DISPLAY_WIDTH
+			+ x] = game->rc->color;
+		y++;
+	}
 }
-
-
 
 uint32_t	create_rgba(int r, int g, int b, int a)
 {
@@ -466,5 +490,3 @@ uint32_t	parse_color(const char *line)
 		print_error("Error: RGB values out of range in parse_color\n");
 	return (create_rgba(r, g, b, 255));
 }
-
-
